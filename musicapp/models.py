ed27a5df92280b8e0720b63cbb3b37e1254e01ef
Paul-Gleeson <Shagamatula@gmail.com>
2023-10-20 19:14:34 +0000

    create musicapp on heroku

--- musicapp/models.py
+++ musicapp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class MusicMod(models.Model):
+    artist_name = models.CharField(max_length=100)
+    song_title = models.CharField(max_length=100)
+    song_file = models.FileField(upload_to='songs/')
+    song_image = models.ImageField(upload_to='images/', null=True, blank=True)
+
+    def __str__(self):
+        return self.song_title
