7696fff2f433bfef6483251b5eaa175ba9161681
Paul-Gleeson <Shagamatula@gmail.com>
2023-10-22 01:50:52 +0000

    song_list and header base html all set up

--- home/views.py
+++ home/views.py
@@ -6,5 +6,5 @@ def index(request):
     return render(request, 'home/index.html')
 
 
-def musicapp(request):
-    return render(request, 'musicapp/musicapp.html')
+# def musicapp(request):
+#     return render(request, 'musicapp/musicapp.html')
