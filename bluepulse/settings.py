0f1fa201725f352c5055ff0e5d682bfd22687411
Paul-Gleeson <Shagamatula@gmail.com>
2023-10-26 12:53:39 +0000

    install menssages

--- bluepulse/settings.py
+++ bluepulse/settings.py
@@ -11,8 +11,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-
 import os
+from django.contrib.messages import constants as messages
 import dj_database_url
 
 if os.path.isfile("env.py"):
@@ -57,6 +57,14 @@ INSTALLED_APPS = [
     'home',
     'musicapp',
 ]
+
+MESSAGE_TAGS = {
+        messages.DEBUG: 'alert-info',
+        messages.INFO: 'alert-info',
+        messages.SUCCESS: 'alert-success',
+        messages.WARNING: 'alert-warning',
+        messages.ERROR: 'alert-danger',
+    }
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
