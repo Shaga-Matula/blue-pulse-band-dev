a86e68ed5dba66307a7804170babbb6bb46d016e
Paul-Gleeson <Shagamatula@gmail.com>
2023-10-27 00:09:24 +0000

    products

--- merchandise/views.py
+++ merchandise/views.py
@@ -4,3 +4,5 @@ from .models import MerchandiseMod
 def product_detail(request, pk):
     product = get_object_or_404(MerchandiseMod, pk=pk)
     return render(request, 'product_detail.html', {'product': product})
+
+
