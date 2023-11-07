edc93c6f2840db8f7c8384054ffc9e4fa9e598a3
Paul-Gleeson <Shagamatula@gmail.com>
2023-11-07 22:46:48 +0000

    feat(bug): fix remove item in bag js path fix: remove /bag/

--- bag/urls.py
+++ bag/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('view_bag', views.view_bag, name='view_bag'),
     path('add/<item_id>/', views.add_to_bag, name='add_to_bag'),
     path('adjust/<item_id>/', views.AdjustBagView.as_view(), name='adjust_bag'),
+    path('remove/<item_id>/', views.remove_from_bag, name='remove_from_bag'),
 ]
