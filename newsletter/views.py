ad9afef2f9381f7ebef1b0e3e37eab24a19f6928
Paul-Gleeson <Shagamatula@gmail.com>
2023-11-25 16:50:53 +0000

    mail testing

--- newsletter/views.py
+++ newsletter/views.py
@@ -1,3 +1,4 @@
+from django.core.mail import send_mail, settings
 from django.shortcuts import redirect, render
 from django.views import View
 
@@ -15,12 +16,23 @@ class SubscribeView(View):
     def post(self, request, *args, **kwargs):
         form = SubscribeForm(request.POST)
         if form.is_valid():
-            form.save()
-            # Need a success message here 
+            instance = form.save(commit=False)  # Do not save to the database yet
+            
+            instance.save()  # Save to the database now
+            # Send email to 'me@me.com'
+            subject = 'Subscription Successful'
+            message = f'A new subscriber with email {instance.email} has subscribed to the newsletter.'
+            from_email = settings.DEFAULT_FROM_EMAIL
+            recipient_list = ['bluepulseband@gmail.com']
+
+            send_mail(subject, message, from_email, recipient_list)
+
+            success_message = "You have successfully subscribed to the newsletter. An email has been sent to 'me@me.com'."
+            messages.success(request, success_message)
+
             return redirect("subscribe_success")
 
         return render(request, self.template_name, {"form": form})
-
 
 class SubscribeSuccessView(View):
     template_name = "newsletter/subscribe_success.html"
